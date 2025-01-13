5099edd4dc8bc3242755bb19d9e23b6f648486d7
Ashok <pavanashok5584@gmail.com>
2025-01-13 16:34:36 +0530

    Backend configuration

--- backend.sh
+++ backend.sh
@@ -62,7 +62,7 @@ VALIDATE $? "app directory creation"
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>> $LOG_FILE_NAME
 VALIDATE $? "app download"
 
-cd /app
+cd /app &>> $LOG_FILE_NAME
 
 unzip /tmp/backend.zip &>> $LOG_FILE_NAME
 VALIDATE $? "unzipping the downloaded"
